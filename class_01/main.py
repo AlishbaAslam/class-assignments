db034a5e6d53f6d1060b53b1fb00240f5c437305
Alishba <162062529+AlishbaAslam@users.noreply.github.com>
2025-03-09 12:54:57 -0700

    Add files via upload

---
+++ class_01/main.py
@@ -0,0 +1,91 @@
+# DATA TYPES IN PYTHON:
+
+# 1:Numeric Data Types:
+
+# Integer (int):
+age = 22
+
+# Float (float): decimal numbers.
+height = 5.3 
+
+# Complex (complex):
+a = 5 + 5j 
+
+# 2:Sequence Data Types:
+
+# String (str):
+text = "Hello, World!"
+
+# List (list): mutable collection.
+list = [3, "Python", 4.16]
+
+# Tuple (tuple): immutable collection.
+tuple = (1, 2, "Python")
+
+# 3:Boolean Data Type:
+
+# Boolean (bool):
+is_student = True
+is_teacher = False
+
+# 4:Set Data Type:
+
+# Set (set): unordered and unique items.
+set = {1, 2, 3, 4,}
+
+# Frozen Set (frozenset): immutable set.
+frozen_set = frozenset({1, 2, 3, 4})
+
+# 5:Dictionary Data Type:
+
+# Dictionary (dict): key-value pairs.
+person = {"name": "Alishba", "age": 22, "city": "Karachi"}
+
+# None Data Type:
+
+# None (None): 
+a = None
+
+# Range (range): sequence of numbers.
+range = range(5)
+
+
+
+
+
+# KEYWORDS IN PYTHON:
+
+# Control Flow Keywords:
+#  if, elif, else, for, while, break, continue, pass, return, yield.
+
+# Exception Handling Keywords:
+# try, except, finally, raise, assert.
+
+# Variable & Scope Keywords:
+# global, nonlocal, del.
+
+# Class and object Keywords:
+# class, def, lambda, self, decorator.
+
+# Boolean & None Keywords:
+# True, False, None.
+
+# Import and Module Keywords:
+# import, from, as, with.
+
+# ASync Programming Keywords:
+# async, await.
+
+# Logical and Comparison Keywords:
+# and, or, not, in, is.
+
+# Miscellaneous Keywords:
+# match, case.
+
+
+
+
+
+
+
+
