207cdcbd96ece13a3d17fbc64a619972817878b0
Alishba <162062529+AlishbaAslam@users.noreply.github.com>
2025-05-11 07:36:35 -0700

    first commit

---
+++ class_04/math-date-time-calender/app.py
@@ -0,0 +1,12 @@
+# Epoch     January 1, 1970, 00:00:00 (UTC)
+# Ticks     unit of time    tick = 1 second
+
+# Epoch   	Starting point of time (usually Jan 1, 1970)
+# Ticks  	Number of seconds passed since the epoch
+
+# Ticks & Epoch
+
+import time
+
+ticks = time.time()
+print("Ticks since 1970:", ticks)
